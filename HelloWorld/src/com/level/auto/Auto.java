705c67bb6b58f05cd572311aac52b709f801d47f
peter <peter@gmail.com>
2013-03-23 22:03:19 +0000

    M_Issue1 - Introduce Auto

---
+++ HelloWorld/src/com/level/auto/Auto.java
@@ -0,0 +1,10 @@
+package com.level.auto;
+
+import com.level.bottom.Countries;
+import com.level.middle.Oceans;
+
+public class Auto {
+	public void inform (){
+		System.out.println("Car - Bus - Train - boat");
+	}
+}
